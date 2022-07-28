6239081b5d9c8778f0c5ee1e4fe9eb8f441a1167
joohe71 <joohe71@gmail.com>
2022-07-28 15:32:43 +0900

    add: 유효성 체크 핸들러 추가 및 버튼 활성화 상태 연결

--- front/src/auth/Login.tsx
+++ front/src/auth/Login.tsx
@@ -4,6 +4,32 @@ import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  // 유저 로그인 데이터
+  const [userData, setUserData] = React.useState({ email: "", password: "" });
+  // 유저 로그인 유효성 검사
+  const [isValid, setIsValid] = React.useState({
+    email: false,
+    password: false,
+  });
+  // 유저 로그인 데이터 변경 함수
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+    if (e.target.name === "email") checkEmail(e.target.value);
+    if (e.target.name === "password") checkPassword(e.target.value);
+    console.log(isValid);
+  };
+  // 이메일 형식 체크
+  const checkEmail = (email: string) => {
+    const regex =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    setIsValid({ ...isValid, email: regex.test(email) });
+  };
+  // 비밀번호 형식 체크
+  const checkPassword = (password: string) => {
+    if (password.length < 8) setIsValid({ ...isValid, password: false });
+    else setIsValid({ ...isValid, password: true });
+  };
+
   return (
     <Layout>
       <Form>
@@ -12,9 +38,12 @@ const Login = () => {
           <Col>
             <label htmlFor="loginid">아이디(E-mail)</label>
             <input
-              type="text"
+              type="email"
               id="loginid"
+              name="email"
               placeholder="아이디(E-mail)을 입력해 주세요"
+              value={userData.email}
+              onChange={handleChange}
             />
           </Col>
           <Col>
@@ -22,7 +51,10 @@ const Login = () => {
             <input
               type="password"
               id="loginpw"
+              name="password"
               placeholder="비밀번호를 입력해 주세요"
+              value={userData.password}
+              onChange={handleChange}
             />
           </Col>
           <div>
@@ -30,7 +62,17 @@ const Login = () => {
             <a onClick={() => navigate("/register")}> 회원가입</a>
           </div>
 
-          <button type="submit">로그인</button>
+          <button
+            type="submit"
+            disabled={!isValid.email || !isValid.password}
+            onClick={() => {
+              alert(`${userData.email} ${userData.password}`);
+              console.log(checkEmail(userData.email));
+              console.log(checkPassword(userData.password));
+            }}
+          >
+            로그인
+          </button>
         </fieldset>
       </Form>
     </Layout>

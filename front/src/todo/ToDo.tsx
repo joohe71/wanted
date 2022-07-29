30a6fb46d5b3d03a92b5ff2ac8598a382ff57329
joohe71 <joohe71@gmail.com>
2022-07-29 23:41:07 +0900

    add: todo navbar, 로그아웃 핸들러 추가

--- front/src/todo/ToDo.tsx
+++ front/src/todo/ToDo.tsx
@@ -1,13 +1,43 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const ToDo = () => {
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    alert("로그아웃 성공");
+    navigate("/login");
+  };
+
   useEffect(() => {
     if (localStorage.getItem("token") === null) navigate("/login");
   });
-  return <div>ToDo</div>;
+  return (
+    <div>
+      <Nav>
+        <h1 id="header">ToDo List</h1>
+        <div id="logout" onClick={handleLogout}>
+          로그아웃
+        </div>
+      </Nav>
+    </div>
+  );
 };
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  #logout {
+    text-align: right;
+    width: 100%;
+    margin-right: 10%;
+    cursor: pointer;
+  }
+`;
 
 export default ToDo;

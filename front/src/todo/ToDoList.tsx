c4ed43cf06a043010701a89c6c6f8af811148836
joohe71 <joohe71@gmail.com>
2022-07-30 22:14:31 +0900

    add&style: 버튼 추가 및 레이아웃 수정

--- front/src/todo/ToDoList.tsx
+++ front/src/todo/ToDoList.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { ToDoProps } from "./ToDo";
+import styled from "styled-components";
 
 const ToDoList = ({ todos }: ToDoProps) => {
   return (
-    <ul>
+    <Ul>
       {todos.map((todo, index) => (
-        <li key={`todo-${index}`}>{todo.title}</li>
+        <Group key={`todo-${index}`}>
+          <li key={`todo-${index}`}>{todo.title}</li>
+          <Group>
+            <button>수정</button>
+            <button>삭제</button>
+          </Group>
+        </Group>
       ))}
-    </ul>
+    </Ul>
   );
 };
+
+const Ul = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const Group = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default ToDoList;

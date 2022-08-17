624bd1d1c570a1aba633a0950c903aaf7b3a85a2
joohe71 <joohe71@gmail.com>
2022-08-17 15:33:34 +0900

    refactor : 커스텀 훅으로 뷰, 로직 분리

--- front/src/auth/Login.tsx
+++ front/src/auth/Login.tsx
@@ -1,55 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import styled from "styled-components";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
   const navigate = useNavigate();
-  // 유저 로그인 데이터
-  const [userData, setUserData] = React.useState({ email: "", password: "" });
-  // 유저 로그인 유효성 검사
-  const [isLoginValid, setIsLoginValid] = React.useState({
-    email: false,
-    password: false,
-  });
-
-  // 유저 로그인 데이터 변경 함수
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserData({ ...userData, [e.target.name]: e.target.value });
-    if (e.target.name === "email") checkEmail(e.target.value);
-    if (e.target.name === "password") checkPassword(e.target.value);
-  };
-
-  // 이메일 형식 체크
-  const checkEmail = (email: string) => {
-    const regex =
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    setIsLoginValid({ ...isLoginValid, email: regex.test(email) });
-  };
-
-  // 비밀번호 형식 체크
-  const checkPassword = (password: string) => {
-    if (password.length < 8)
-      setIsLoginValid({ ...isLoginValid, password: false });
-    else setIsLoginValid({ ...isLoginValid, password: true });
-  };
-
-  // 제출 버튼 클릭 이벤트
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(
-        "http://localhost:8080/users/login",
-        userData
-      );
-      await localStorage.setItem("token", res.data.token);
-      await alert(res.data.message);
-      await navigate("/");
-      await console.log("로그인 성공");
-    } catch {
-      alert("로그인 실패");
-    }
-  };
+  const { userData, handleChange, handleSubmit, isLoginValid } = useLogin();
 
   return (
     <Layout>
--- front/src/auth/SignUp.tsx
+++ front/src/auth/SignUp.tsx
@@ -1,69 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import useSignUp from "../hooks/useSignUp";
 
 const SignUp = () => {
   const navigate = useNavigate();
-  // 유저 회원가입 데이터
-  const [registerData, setRegisterData] = React.useState({
-    email: "",
-    password: "",
-    passwordCheck: "",
-  });
-  // 유저 회원가입 유효성 검사
-  const [isRegisterValid, setIsRegisterValid] = React.useState({
-    email: false,
-    password: false,
-    passwordCheck: false,
-  });
-
-  // 유저 회원가입 데이터 변경 함수
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRegisterData({ ...registerData, [e.target.name]: e.target.value });
-    if (e.target.name === "email") checkEmail(e.target.value);
-    if (e.target.name === "password") checkPassword(e.target.value);
-    if (e.target.name === "passwordCheck") isSamePassword(e.target.value);
-  };
-
-  // 이메일 형식 체크
-  const checkEmail = (email: string) => {
-    const regex =
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    setIsRegisterValid({ ...isRegisterValid, email: regex.test(email) });
-  };
-
-  // 비밀번호 형식 체크
-  const checkPassword = (password: string) => {
-    if (password.length < 8)
-      setIsRegisterValid({ ...isRegisterValid, password: false });
-    else setIsRegisterValid({ ...isRegisterValid, password: true });
-  };
-
-  // 비밀번호 일치 체크
-  const isSamePassword = (passwordCheck: string) => {
-    if (passwordCheck === registerData.password)
-      setIsRegisterValid({ ...isRegisterValid, passwordCheck: true });
-    else setIsRegisterValid({ ...isRegisterValid, passwordCheck: false });
-  };
-
-  // 등록 버튼 클릭 이벤트
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const { email, password } = registerData;
-      const res = await axios.post("http://localhost:8080/users/create", {
-        email,
-        password,
-      });
-
-      await alert(res.data.message);
-      await navigate("/login");
-    } catch {
-      alert("회원가입 실패");
-    }
-  };
-
+  const { registerData, isRegisterValid, handleChange, handleSubmit } =
+    useSignUp();
   return (
     <Layout>
       <Form onSubmit={handleSubmit}>
